2c6fbf8da18e9c2e5a34e991c0ec850f92b59582
waris-00 <wariswasay@gmail.com>
2025-05-07 17:50:56 +0100

    Add Welcome Web App

---
+++ hello_flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask #import 
+import MySQLdb
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="mydb2",    # Hostname of the MySQL container, they connect by anmes instead of IP addresses
+        user="root",    # Username to connect to MySQL
+        passwd="my-secret-pw",  # Password for the MySQL user
+        db="mysql"      # Name of the database to connect to
+    )
+    cur = db.cursor()   #
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, World! MySQL version: {version[0]}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
